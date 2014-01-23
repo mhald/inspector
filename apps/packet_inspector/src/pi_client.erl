29e6320d14c8f4a5f1c21d578a82fa5492e00e65
Martin Hald <mhald@mac.com>
2014-01-23 07:28:34 -0800

    updated pi_client

--- apps/packet_inspector/src/pi_client.erl
+++ apps/packet_inspector/src/pi_client.erl
@@ -9,15 +9,18 @@
          handle_event/3,
          handle_info/3,
          handle_sync_event/4,
-         start_link/2]).
+         start_link/1]).
 
--export([send_packet/2,
-         receive_packet/2,
+-export([send_packet/3,
+         send_packet/5,
+         receive_packet/3,
+         receive_packet/5,
          http_request/4,
          http_response/5,
          add_sessions/1,
          remove_sessions/1,
-         encode/1]).
+         encode/1,
+         is_tracing_on/0]).
 
 -export([
         connected/2,
@@ -34,10 +37,13 @@
         account_callback_fun :: atom()
     }).
 
--spec start_link(atom(), atom()) -> ok.
-start_link(Mod, Fun) -> gen_fsm:start_link({local, ?MODULE}, ?MODULE, {Mod, Fun}, []).
+-define(IS_TRACING_ON, true).
+
+-spec start_link([any()]) -> ok.
+start_link(Params) -> gen_fsm:start_link({local, ?MODULE}, ?MODULE, Params, []).
 
 -type account() :: string().
+-type encoding() :: binary().
 -type data() :: string().
 
 -type url() :: string() | binary().
@@ -56,22 +62,25 @@ start_link(Mod, Fun) -> gen_fsm:start_link({local, ?MODULE}, ?MODULE, {Mod, Fun}
 -type code() :: non_neg_integer().
 -type body() :: string() | binary().
 
--spec receive_packet(account()|undefined, data()) -> ok.
-receive_packet(Account, _) when is_atom(Account) -> ok;
-receive_packet(Account, Data) when is_list(Account) -> 
-    gen_fsm:send_event(?MODULE, {receive_packet, Account, Data}).
+-spec receive_packet(account()|undefined, encoding(), data()) -> ok.
+receive_packet(Account, _, _) when is_atom(Account) -> ok;
+receive_packet(Account, Encoding, Data) -> receive_packet(Account, Encoding, undefined, undefined, Data).
+receive_packet(Account, Encoding, Sender, Receiver, Data) when is_list(Account) -> 
+    gen_fsm:send_event(?MODULE, {receive_packet, Account, Encoding, Sender, Receiver, Data}).
 
--spec send_packet(account()|undefined, data()) -> ok.
-send_packet(undefined, _) -> ok;
-send_packet(Account, Data) when is_list(Account) -> gen_fsm:send_event(?MODULE, {send_packet, Account, Data}).
+-spec send_packet(account()|undefined, encoding(), data()) -> ok.
+send_packet(undefined, _, _) -> ok;
+send_packet(Account, Encoding, Data) -> send_packet(Account, Encoding, undefined, undefined, Data).
+send_packet(Account, Encoding, Sender, Reciever, Data) when is_list(Account) ->
+    gen_fsm:send_event(?MODULE, {send_packet, Account, Encoding, Sender, Reciever, Data}).
 
 -spec http_request(account(), url(), headers(), parameters()) -> ok.
 http_request(undefined, _Url, _Headers, _Parameters) -> ok;
-http_request(Account, Url, Headers, Parameters) when is_list(Account) -> gen_fsm:send_event(?MODULE, encode({http_request, Account, Url, Headers, Parameters})).
+http_request(Account, Url, Headers, Parameters) when is_list(Account) -> gen_fsm:send_event(?MODULE, {http_request, Account, Url, Headers, Parameters}).
 
 -spec http_response(account(), url(), code(), headers(), body()) -> ok.
 http_response(undefined, _Url, _Code, _Headers, _Body) -> ok;
-http_response(Account, Url, Code, Headers, Body) when is_list(Account) -> gen_fsm:send_event(?MODULE, encode({http_response, Account, Url, Code, Headers, Body})).
+http_response(Account, Url, Code, Headers, Body) when is_list(Account) -> gen_fsm:send_event(?MODULE, {http_response, Account, Url, Code, Headers, Body}).
 
 -spec add_sessions([userdata()]) -> ok.
 add_sessions(Accounts) -> gen_fsm:send_event(?MODULE, {add_sessions, Accounts}).
@@ -79,9 +88,12 @@ add_sessions(Accounts) -> gen_fsm:send_event(?MODULE, {add_sessions, Accounts}).
 -spec remove_sessions([userid()]) -> ok.
 remove_sessions(Accounts) -> gen_fsm:send_event(?MODULE, {remove_sessions, Accounts}).
 
--spec init({atom(), atom()}) -> {ok, #state{}}.
-init({Mod, Fun}) ->
-    {ok, disconnected, #state{host="localhost", port=8001, account_callback_mod=Mod, account_callback_fun=Fun}}.
+-spec init([any()]) -> {ok, #state{}}.
+init(Params) ->
+    %% TODO: exit with error if they parameters are not given
+    {Mod, Fun} = proplists:get_value(account_info_callback, Params),
+    {Host, Port} = proplists:get_value(host, Params),
+    {ok, disconnected, #state{host=Host, port=Port, account_callback_mod=Mod, account_callback_fun=Fun}}.
 
 -spec encode(Request::term()) -> {pi_packet, string()}.
 encode({http_request, Account, Url, Headers, Parameters}) ->
@@ -155,20 +167,26 @@ avatar(Link) -> {<<"avatar">>, as_binary(Link)}.
 -spec connected(Request::term(), State::term()) ->
     {next_state, NextStateName::atom(), NewState::term()} |
     {stop, Reason::term(), NewState::term()}.
-connected({send_packet, Account, Text}, #state{socket=Socket, queue=[]} = State) ->
-    Json = standard_pi_account_packet(<<"receive_packet">>, Account, [
-            {<<"direction">>, <<"received">>},
-            {<<"encoding">>, <<"xmpp">>},
+connected({send_packet, Account, Encoding, Sender, Reciever, Text}, #state{socket=Socket, queue=[]} = State) ->
+    Json = standard_pi_account_packet(<<"data_packet">>, Account, [
+            {<<"direction">>, <<"sent">>},
+            {<<"encoding">>, Encoding},
             {<<"data">>, list_to_binary(Text)}
         ], State),
     ok = gen_tcp:send(Socket, [Json, $\n]),
     {next_state, connected, State};
-connected({received_packet, Account, Text}, #state{socket=Socket, queue=[]} = State) ->
-    Json = standard_pi_account_packet(<<"receive_packet">>, Account, [
+connected({receive_packet, Account, Encoding, Sender, Receiver, Text}, #state{socket=Socket, queue=[]} = State) ->
+    Extra = case [Sender, Receiver] of
+        [undefined, _] -> [];
+        [_, undefined] -> [];
+        [Sender, Receiver] -> 
+            [{<<"sender">>, Sender}, {<<"receiver">>, Receiver}, {<<"on-behalf-of">>, true}]
+    end,
+    Json = standard_pi_account_packet(<<"data_packet">>, Account, [
             {<<"direction">>, <<"received">>},
-            {<<"encoding">>, <<"xmpp">>},
+            {<<"encoding">>, Encoding},
             {<<"data">>, list_to_binary(Text)}
-        ], State),
+        ] ++ Extra, State),
     ok = gen_tcp:send(Socket, [Json, $\n]),
     {next_state, connected, State};
 connected({add_sessions, Accounts}, #state{socket=Socket} = State) ->
@@ -186,19 +204,42 @@ connected({remove_sessions, Accounts}, #state{socket=Socket} = State) ->
             {<<"accounts">>, Accounts_Data}
         ]),
     ok = gen_tcp:send(Socket, [Text, $\n]),
-    {next_state, connected, State#state{}}.
+    {next_state, connected, State#state{}};
+connected({http_request, Account, Url, Headers, Parameters}, #state{socket=Socket, queue=[]} = State) ->
+    Json = standard_pi_account_packet(<<"http_request">>, Account, [
+            {<<"direction">>, <<"received">>},
+            {<<"encoding">>, <<"http">>},
+            {<<"timestamp">>, now_for_timestamp()},
+            {<<"url">>, as_binary(Url)},
+            {<<"headers">>, proplist_to_json(Headers)},
+            {<<"parameters">>, proplist_to_json(Parameters)}
+        ], State),
+    ok = gen_tcp:send(Socket, [Json, $\n]),
+    {next_state, connected, State};
+connected({http_response, Account, Url, Code, Headers, Body}, #state{socket=Socket, queue=[]} = State) ->
+    Json = standard_pi_account_packet(<<"http_response">>, Account, [
+            {<<"direction">>, <<"sent">>},
+            {<<"encoding">>, <<"http">>},
+            {<<"timestamp">>, now_for_timestamp()},
+            {<<"url">>, as_binary(Url)},
+            {<<"code">>, Code},
+            {<<"headers">>, proplist_to_json(Headers)},
+            {<<"body">>, as_binary(Body)}
+        ], State),
+    ok = gen_tcp:send(Socket, [Json, $\n]),
+    {next_state, connected, State}.
 
 -spec disconnected(Request::term(), State::term()) ->
     {next_state, NextStateName::atom(), NewState::term()} |
     {stop, Reason::term(), NewState::term()}.
 disconnected(unknown_packet, State) -> {next_data, disconnected, State};
 disconnected({pi_packet, Text}, State) -> connect_and_process({pi_packet, Text}, State);
-disconnected({send_packet, Account, Text}, State) -> connect_and_process({send_packet, Account, Text}, State);
-disconnected({received_packet, Account, Text}, State) -> connect_and_process({received_packet, Account, Text}, State);
+disconnected({send_packet, Account, Encoding, Text}, State) -> connect_and_process({send_packet, Account, Encoding, Text}, State);
+disconnected({receive_packet, Account, Encoding, Text}, State) -> connect_and_process({received_packet, Account, Encoding, Text}, State);
 disconnected({add_sessions, Text}, State) -> connect_and_process({pi_packet, Text}, State);
 disconnected({remove_sessions, Text}, State) -> connect_and_process({pi_packet, Text}, State);
 disconnected(Other, State) ->
-    lager:warning("Unknown request when disconnected ~p", [Other]),
+    lager:debug("Unknown request when disconnected ~p", [Other]),
     {next_state, disconnected, State}.
 
 connect_and_process(_Request, #state{} = State) -> %% todo: fix that the fist request is thrown away
@@ -215,7 +256,7 @@ loop_until_connected(State) ->
     end.
 
 reconnect(#state{host=Host, port=Port} = _State) ->
-    lager:warning("Connecting  to ~p:~p", [Host, Port]),
+    lager:info("Connecting  to ~p:~p", [Host, Port]),
     case gen_tcp:connect(Host, Port, [binary]) of
         {ok, Socket} ->
             lager:info("Connected socket to packet inspector"),
@@ -270,6 +311,7 @@ now_for_timestamp() ->
     {MegaSecs,Secs,MicroSecs} = now(),
     (MegaSecs*1000000 + Secs)*1000000 + MicroSecs.
 
+get_account_data(undefined, _State) -> [];
 get_account_data(Account, #state{account_callback_mod=Mod, account_callback_fun=Fun} = _State) ->
     case get(Account) of
         Account_Data when Account_Data =/= undefined -> Account_Data;
@@ -297,3 +339,6 @@ standard_pi_account_packet(Type, Account, Data, State) ->
                             {<<"data">>, {Data}}
                         ]}
                 }]}).
+
+is_tracing_on() ->
+    ?IS_TRACING_ON.
